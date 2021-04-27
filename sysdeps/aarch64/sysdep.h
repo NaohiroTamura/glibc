c582917071e76cfed84fafb0c82cb70339294386
Naohiro Tamura <naohirot@jp.fujitsu.com>
2021-04-27 20:18:13 +0900

    aarch64: define BTI_C and BTI_J macros only if HAVE_AARCH64_BTI

--- sysdeps/aarch64/sysdep.h
+++ sysdeps/aarch64/sysdep.h
@@ -62,8 +62,13 @@ strip_pac (void *p)
 #define ASM_SIZE_DIRECTIVE(name) .size name,.-name
 
 /* Branch Target Identitication support.  */
+#if HAVE_AARCH64_BTI
 #define BTI_C		hint	34
 #define BTI_J		hint	36
+#else
+#define BTI_C
+#define BTI_J
+#endif
 
 /* Return address signing support (pac-ret).  */
 #define PACIASP		hint	25
